a23e145de0dd3e4966369f22427b35d1d5a58761
Rizwanudeen K <rizwankendo@gmail.com>
2024-06-17 07:13:43 +0000

    XSS vulnerability explained in comments

--- _templates/_commentsResult.php
+++ _templates/_commentsResult.php
@@ -44,7 +44,16 @@ $comments = Database::getComment();
                 echo '<div>';
                 echo '<strong>' . $comment['username'] . ':</strong> ' . $comment['comment'];
                 echo '</div>';
-                
+                /*
+                * The user input from the comment section is not validated and unsanitized.
+                * Due to that, XSS scripts(Javascript codes in usual) can be included in the comments.
+                * When echoing the comment from Database, the user input JS script will be executed on the webpage.
+                * Example script is <script>alert("Vulnerable to XSS")</script> will display an alertbox with text "Vulnerable to XSS".
+                * Using this cookie hijacking can be done with this script, <script>alert(document.cookie)</script>
+                * The above script will display all the cookies stored by the site in an alert box.
+                * Then the cookies can be sent to the attacker's device in many ways.
+                * site link: https://cng.selfmade.one/
+                 */
                 
                 // Display timestamp near delete button
                 echo '<div class="d-flex align-items-center">';
